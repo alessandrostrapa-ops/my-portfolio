cc8da80b20ee3aa32ad4116e4dc8ba690c7532bb
alessandrostrapa-ops <alessandrostrapa@gmail.com>
2025-10-27 22:33:52 -0300

    Prepare for GitHub Pages deploy, remove dark mode

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ⚠️ Agregá esto para GitHub Pages
 export default defineConfig({
   plugins: [react()],
+  base: '/my-portfolio/' // <--- exacto al nombre del repo
 })
